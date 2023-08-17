5d77527b99f47c59ba9a8982994fb01585bcca52
editor-taka <takachan1050150@gmail.com>
2023-08-17 17:36:14 +0900

    promise

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,17 +1,44 @@
 console.log('Welcome to Node Tutorial')
 
 const http = require("http")
+const {readFile, writeFile} = require("fs")
+const util = require("util")
+const readFilePromise = util.promisify(readFile)
+
+const start = async () =>{
+  try{
+    // const first = await getText("./content/first.txt")
+    // const second = await getText("./content/second.txt")
+    const first = await readFilePromise("./content/first.txt", "utf-8")
+    const second = await readFilePromise("./content/second.txt","utf-8")
+    console.log(first, second)
+  }
+  catch(e){
+    console.log(e)
+  }
+
+}
+
+const getText = (path)=>{
+  return new Promise((resolve, reject) =>{
+    readFile(path, "utf-8", (err, data) =>{
+      if(err){
+        reject(err)
+      }
+      else{
+        resolve(data)
+      }
+    })
+  })
+}
+
+start()
 
 const server = http.createServer((req, res) =>{
  if(req.url === "/"){
   res.end("HOME PAGE")
  }
  if(req.url === "/about"){
-  for(let i =0; i < 10; i++){
-    for(let j = 0; j < 10; j++){
-      console.log(`${i} ${j}`)
-    }
-  }
   res.end("About Page")
  }
 })
