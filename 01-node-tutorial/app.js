04325fa9331e5edf7173170e06c844ff61e45484
editor-taka <takachan1050150@gmail.com>
2023-08-17 15:54:00 +0900

    server with blocking

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,5 +1,21 @@
 console.log('Welcome to Node Tutorial')
 
-const data = require("./06-alternative-flavor")
-require("./07-mind-grenade")
-console.log(data)
+const http = require("http")
+
+const server = http.createServer((req, res) =>{
+ if(req.url === "/"){
+  res.end("HOME PAGE")
+ }
+ if(req.url === "/about"){
+  for(let i =0; i < 10; i++){
+    for(let j = 0; j < 10; j++){
+      console.log(`${i} ${j}`)
+    }
+  }
+  res.end("About Page")
+ }
+})
+
+server.listen(5000, ()=>{
+ console.log("Server Listening on port 5000")
+})
