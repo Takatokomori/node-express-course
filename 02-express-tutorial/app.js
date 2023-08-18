4eaf9fd195faf8dec2fa24a26cc6257da4ae94ba
editor-taka <takachan1050150@gmail.com>
2023-08-18 11:01:20 +0900

    finish basic

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,19 @@
 console.log('Express Tutorial')
+
+const http = require("http")
+const server = http.createServer((req, res)=>{
+  console.log("user hit the server")
+  const url = req.url;
+  if(url === "/"){
+    res.writeHead(200, { "content-type": "text/html" })
+    res.write("<h1>Home Page</h1>")
+    res.end()
+  }
+  else{
+    res.writeHead(404, { "content-type": "text/html" })
+    res.write("<h1>Not Found</h1>")
+    res.end()
+  }
+})
+
+server.listen(5000)
