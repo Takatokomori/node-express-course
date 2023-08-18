dfab693f5713c09e98957830d259f3fef3136f5c
editor-taka <takachan1050150@gmail.com>
2023-08-18 12:24:03 +0900

    express basic

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,43 +1,30 @@
 console.log('Express Tutorial')
+const express = require("express")
+const app = express()
 
-const http = require("http")
-const {readFileSync} = require("fs")
-
-// home page
-const homePage = readFileSync("./navbar-app/index.html")
-const homeStyles = readFileSync("./navbar-app/styles.css")
-const homeImage = readFileSync("./navbar-app/logo.svg")
-const homeLogic = readFileSync("./navbar-app/browser-app.js")
-
-const server = http.createServer((req, res)=>{
-  console.log("user hit the server")
-  const url = req.url;
-  
-  if(url === "/"){
-    res.writeHead(200, { "content-type": "text/html" })
-    res.write(homePage)
-    res.end()
-  }
-  else if(url === "/styles.css"){
-    res.writeHead(200, { "content-type": "text/css" })
-    res.write(homeStyles)
-    res.end()
-  }
-  else if(url === "/logo.svg"){
-    res.writeHead(200, { "content-type": "image/svg+xml" })
-    res.write(homeImage)
-    res.end()
-  }
-  else if(url === "/browser-app.js"){
-    res.writeHead(200, { "content-type": "text/javascript" })
-    res.write(homeLogic)
-    res.end()
-  }
-  else{
-    res.writeHead(404, { "content-type": "text/html" })
-    res.write("<h1>Not Found</h1>")
-    res.end()
-  }
+app.get("/", (req, res)=>{
+  console.log("User hit the resource")
+  res.status(200).end("Home")
 })
 
-server.listen(5000)
+app.get("/about", (req, res)=>{
+  res.status(200).send("About Page")
+})
+
+app.all("*", (req, res)=>{
+  res.status(404).send("<h1>resource not found</h1>")
+})
+
+
+app.listen(5000, ()=>{
+  console.log("server is listening on port 5000")
+})
+
+
+// app.get
+// app.post
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.listen
