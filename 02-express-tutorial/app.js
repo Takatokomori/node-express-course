5c0e071efdb19a223a539d4bc2b0f87154075061
editor-taka <takachan1050150@gmail.com>
2023-08-18 13:46:17 +0900

    express public show

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,20 +1,18 @@
 console.log('Express Tutorial')
 const express = require("express")
 const app = express()
+const path = require("path")
 
-app.get("/", (req, res)=>{
-  console.log("User hit the resource")
-  res.status(200).end("Home")
+// set up static middleware
+app.use(express.static("./public"))
+
+// app.get("/", (req, res)=>{
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"))
+// })
+
+app.get("*", (req, res)=>{
+  res.status(404).send("resource not found")
 })
-
-app.get("/about", (req, res)=>{
-  res.status(200).send("About Page")
-})
-
-app.all("*", (req, res)=>{
-  res.status(404).send("<h1>resource not found</h1>")
-})
-
 
 app.listen(5000, ()=>{
   console.log("server is listening on port 5000")
