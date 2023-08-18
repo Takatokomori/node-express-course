4b6a4a80cddc9bd344579b2b97b62ec252fc97d4
editor-taka <takachan1050150@gmail.com>
2023-08-18 12:26:45 +0900

    express basic

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,48 +1,14 @@
 console.log('Welcome to Node Tutorial')
 
-const http = require("http")
-const {readFile, writeFile} = require("fs")
-const util = require("util")
-const readFilePromise = util.promisify(readFile)
+var http = require("http")
+var fs = require("fs")
 
-const start = async () =>{
-  try{
-    // const first = await getText("./content/first.txt")
-    // const second = await getText("./content/second.txt")
-    const first = await readFilePromise("./content/first.txt", "utf-8")
-    const second = await readFilePromise("./content/second.txt","utf-8")
-    console.log(first, second)
-  }
-  catch(e){
-    console.log(e)
-  }
-
-}
-
-const getText = (path)=>{
-  return new Promise((resolve, reject) =>{
-    readFile(path, "utf-8", (err, data) =>{
-      if(err){
-        reject(err)
-      }
-      else{
-        resolve(data)
-      }
-    })
+http.createServer((req, res)=>{
+  const fileStream = fs.createReadStream("./content/result-sync.txt")
+  fileStream.on("open", ()=>{
+    fileStream.pipe(res)
   })
-}
-
-start()
-
-const server = http.createServer((req, res) =>{
- if(req.url === "/"){
-  res.end("HOME PAGE")
- }
- if(req.url === "/about"){
-  res.end("About Page")
- }
-})
-
-server.listen(5000, ()=>{
- console.log("Server Listening on port 5000")
+  fileStream.on("error", (error)=>{
+    res.end(error)
+  })
 })
